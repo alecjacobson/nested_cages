fa09aad01e4bb63d56c0007dc8f5c09553eacdf4
Leonardo Sacht <Leo@h184-18.wireless.ufsc.br>
2015-01-06 10:13:36 -0200

    updated results script
    Former-commit-id: 1644ac09e383f8517f05ae2e83e8a057d0e35979
    Former-commit-id: b4a6bb649f5195f4c5dcea0bb3ad3be7006852a9

--- mission_control/results_script_2015_01_05.m
+++ mission_control/results_script_2015_01_05.m
@@ -7,7 +7,7 @@
 % [V0,F0] = readOBJ('../../Meshes/Results/anchor_volume/anchor.obj');
 % [cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-12/3)*floor(size(F0,1)) 2^(-10/3)*floor(size(F0,1)) 2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))],'eps_distance',1e-3);
 % save('../../Meshes/Results/gargo_volume.mat','cages_V','cages_F','Pall','V_coarse','F_coarse','V0','F0','timing');
-% % Obs.: OK, using eps = 1e-3
+% % Obs.: OK, using eps = 1e-3. Saved it wrong as gargo
 
 % [V0,F0] = readOBJ('../../Meshes/Results/couplingdown_volume/couplingdown.obj');
 % [cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))],'eps_distance',5e-3);
@@ -19,13 +19,13 @@
 % save('../../Meshes/Results/gargo_volume.mat','cages_V','cages_F','Pall','V_coarse','F_coarse','V0','F0','timing');
 % % Obs.: OK, using eps = 1e-3
 
-[V0,F0] = readOBJ('../../Meshes/Results/pelvis_volume/pelvis.obj');
-[cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-14/3)*floor(size(F0,1)) 2^(-12/3)*floor(size(F0,1)) 2^(-10/3)*floor(size(F0,1)) 2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))]);
-save('../../Meshes/Results/pelvis_volume.mat','cages_V','cages_F','Pall','V_coarse','F_coarse','V0','F0','timing');
-% Obs.:
+% [V0,F0] = readOBJ('../../Meshes/Results/pelvis_volume/pelvis.obj');
+% [cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-14/3)*floor(size(F0,1)) 2^(-12/3)*floor(size(F0,1)) 2^(-10/3)*floor(size(F0,1)) 2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))]);
+% save('../../Meshes/Results/pelvis_volume.mat','cages_V','cages_F','Pall','V_coarse','F_coarse','V0','F0','timing');
+% % Obs.: OK
 
 [V0,F0] = readOBJ('../../Meshes/Results/bunny_volume/bunny.obj');
-[cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-14/3)*floor(size(F0,1)) 2^(-12/3)*floor(size(F0,1)) 2^(-10/3)*floor(size(F0,1)) 2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))]);
+[cages_V,cages_F,Pall,V_coarse,F_coarse,timing] = multires_per_layer(V0,F0,[2^(-14/3)*floor(size(F0,1)) 2^(-12/3)*floor(size(F0,1)) 2^(-10/3)*floor(size(F0,1)) 2^(-8/3)*floor(size(F0,1)) 2^(-6/3)*floor(size(F0,1)) floor(2^(-4/3)*size(F0,1)) floor(2^(-2/3)*size(F0,1))],'eps_distance',5e-4);
 save('../../Meshes/Results/bunny_volume.mat','cages_V','cages_F','Pall','V_coarse','F_coarse','V0','F0','timing');
 % Obs.:
 
