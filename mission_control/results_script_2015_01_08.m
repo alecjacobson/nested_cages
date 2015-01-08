da85bfec3be7778f012675624cd305b6ae6e8cc0
Leonardo Sacht <Leo@Leonardos-MacBook-Pro.local>
2015-01-08 09:47:39 -0200

    volumetric arap script to run on imac
    Former-commit-id: 56efe3a9000e619e1f14d5c96c0deeb6fcdad59b
    Former-commit-id: c4f1611669d1fc6dc26be0895e8a9c4d76b1924b

--- mission_control/results_script_2015_01_08.m
+++ mission_control/results_script_2015_01_08.m
@@ -11,7 +11,7 @@ levels = floor(2.^((-14:2:-4)/3)*size(F0,1));
   'FinalEnergy','none', ...
   'BetaInit',1e-2, ...
   'Eps',1e-3);
-write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/anchor_volumetric_arap/anchor');
+write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/anchor_volumetric_arap/anchor',cages_V,cages_F);
 % Obs.:
 
 [V0,F0] = load_mesh('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/anchor_volume/arma_0.obj');
@@ -28,7 +28,7 @@ levels = floor(2.^((-14:2:-4)/3)*size(F0,1));
 write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/arma_volumetric_arap/arma');
 % Obs.:
 
-[V0,F0] = load_mesh('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/anchor_volume/bunny_0.obj');
+[V0,F0] = load_mesh('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/anchor_volume/bunny_0.obj',cages_V,cages_F);
 levels = floor(2.^((-14:2:-4)/3)*size(F0,1));
 [cages_V,cages_F,Pall] = ...
   multires_per_layer( ...
@@ -42,7 +42,7 @@ levels = floor(2.^((-14:2:-4)/3)*size(F0,1));
 write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/bunny_volumetric_arap/bunny');
 % Obs.:
 
-[V0,F0] = load_mesh('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/gargo_volume/gargo_0.obj');
+[V0,F0] = load_mesh('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/gargo_volume/gargo_0.obj',cages_V,cages_F);
 levels = floor(2.^((-14:2:-2)/3)*size(F0,1));
 [cages_V,cages_F,Pall] = ...
   multires_per_layer( ...
@@ -53,5 +53,5 @@ levels = floor(2.^((-14:2:-2)/3)*size(F0,1));
   'FinalEnergy','none', ...
   'BetaInit',1e-2, ...
   'Eps',1e-3);
-write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/gargo_volumetric_arap/gargo');
+write_cages('/Users/leokollersacht/Documents/nested_cages/Meshes/Results/gargo_volumetric_arap/gargo',cages_V,cages_F);
 % Obs.:
