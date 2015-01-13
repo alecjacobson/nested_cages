bf1137b8a8cd7d8cce12fa2b2847c5f253227cac
Alec Jacobson <alecjacobson@gmail.com>
2015-01-12 19:45:14 -0500

    performance bug fix in grad_quad
    Former-commit-id: ed33d654195c52371d7ac8a3f26b501261d8a04d
    Former-commit-id: bfcf938cc8b538620f90b3df75b7f376fcc40c63

--- Code/matlab/grad_quadrature_to_vertices.m
+++ Code/matlab/grad_quadrature_to_vertices.m
@@ -24,232 +24,250 @@ function A = grad_quadrature_to_vertices(V0,F0,area_initial,quadrature_order)
   %      elseif (quadrature_order==3)
   %        (#vertices)x(4*#faces) that converts gradient at 3rd order 
   %        quadrature points to vertices on the mesh
-  
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  % Check for cached result, do NOT edit variables until cache is checked,
-  % your function code comes later. See below
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  [cache_exists,cache_name] = find_cache();
-  if cache_exists
-    fprintf('Using cache...\n');
-    load(cache_name);
-    return;
-  end
-  fprintf('First time. Creating cache...\n');
-  
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% Check for cached result, do NOT edit variables until cache is checked,
+  %% your function code comes later. See below
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %[cache_exists,cache_name] = find_cache();
+  %if cache_exists
+  %  fprintf('Using cache...\n');
+  %  load(cache_name);
+  %  return;
+  %end
+  %fprintf('First time. Creating cache...\n');
+
   if (quadrature_order==1)
       % initialize matrix A (this should be in a separate function)
 %       A = sparse(size(V0,1),size(F0,1));
-      
+
       I = zeros(10*size(V0,1),1);
       J = zeros(10*size(V0,1),1);
       Val = zeros(10*size(V0,1),1);
       nnz_cur = 0;
-      
+      warning('Using O(N^2) implementation. Please rewrite');
+
       % defines A entries
       for i=1:size(V0,1)
-          
+
           face_idx = mod(find(F0==i)-1,size(F0,1))+1;
 %           A(i,face_idx) = area_initial(face_idx)/3;
-          
+
           num_entries = size(face_idx,1);
           I(nnz_cur+1:nnz_cur+num_entries) = i;
           J(nnz_cur+1:nnz_cur+num_entries) = face_idx;
           Val(nnz_cur+1:nnz_cur+num_entries) = area_initial(face_idx)/3;
-          
+
           nnz_cur = nnz_cur + num_entries;
-          
+
       end
-      
+
       I = nonzeros(I);
       J = nonzeros(J);
       Val = nonzeros(Val);
       A = sparse(I,J,Val);
-            
+
   elseif (quadrature_order==2)
-      
+    n = size(V0,1);
+    m = size(F0,1);
+    area = doublearea(V0,F0)/2;
+    I = reshape((1:3*m)',m,3);
+    % P12 and P31 depend on F0(:,1)
+    % P23 and P12 depend on F0(:,2)
+    % P31 and P23 depend on F0(:,3)
+    A = ...
+      sparse( ...
+        [ F0(:,[1 1]) F0(:,[2 2]) F0(:,[3 3])], ...
+        [  I(:,[1 3])  I(:,[2 1])  I(:,[3 2])], ...
+        repmat(area/6,1,6), ...
+        n,3*m);
+    return;
+    error('This code should not be used');
+
       % initialize matrix A (this should be in a separate function)
 %       A = sparse(size(V0,1),3*size(F0,1));
-      
+
       I = zeros(3*10*size(V0,1),1);
       J = zeros(3*10*size(V0,1),1);
       Val = zeros(3*10*size(V0,1),1);
       nnz_cur = 0;
-      
+
       % defines A entries
       for i=1:size(V0,1)
-          
+
+          % THIS IS O(N) MAKING THIS WHOLE FUNCTION O(N²)!!!
           face_idx = mod(find(F0==i)-1,size(F0,1))+1;
           % find index of each vertex in the faces it belongs (to
           % select the write quadrature points)
           vtx_idx = mod(find((F0(face_idx,:))'==i)-1,3)+1;
-          
+
           for m=1:size(vtx_idx,1)
-              
+
               if (vtx_idx(m)==1)
 %                   A(i,face_idx(m)) = A(i,face_idx(m))+area_initial(face_idx(m))/6;
 %                   A(i,2*size(F0,1)+face_idx(m)) = A(i,2*size(F0,1)+face_idx(m))+area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = face_idx(m);
                   Val(nnz_cur+1) = area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = 2*size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = area_initial(face_idx(m))/6;
-                  
+
               elseif (vtx_idx(m)==2)
 %                   A(i,face_idx(m)) = A(i,face_idx(m)) + area_initial(face_idx(m))/6;
 %                   A(i,size(F0,1)+face_idx(m)) = A(i,size(F0,1)+face_idx(m)) + area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = face_idx(m);
                   Val(nnz_cur+1) = area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = area_initial(face_idx(m))/6;
-                  
+
               elseif (vtx_idx(m)==3)
 %                   A(i,size(F0,1)+face_idx(m)) = A(i,size(F0,1)+face_idx(m)) + area_initial(face_idx(m))/6;
 %                   A(i,2*size(F0,1)+face_idx(m)) = A(i,2*size(F0,1)+face_idx(m)) + area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = size(F0,1)+face_idx(m);
                   Val(nnz_cur+1) = area_initial(face_idx(m))/6;
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = 2*size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = area_initial(face_idx(m))/6;
-                  
+
               else
-                  
+
                   error('vertex index is not 1 nor 2 nor 3')
-                  
+
               end
-              
+
               nnz_cur = nnz_cur+2;
-              
+
           end
-          
+
       end
-      
+
       I = nonzeros(I);
       J = nonzeros(J);
       Val = nonzeros(Val);
       A = sparse(I,J,Val);
-      
-      
+
+
   elseif (quadrature_order==3)
-      
+      warning('Using O(N^2) implementation. Please rewrite');
+
       % initialize matrix A
 %       A = sparse(size(V0,1),4*size(F0,1));
-      
+
       I = zeros(4*10*size(V0,1),1);
       J = zeros(4*10*size(V0,1),1);
       Val = zeros(4*10*size(V0,1),1);
       nnz_cur = 0;
-      
+
       % defines A entries
       for i=1:size(V0,1)
-          
+
           face_idx = mod(find(F0==i)-1,size(F0,1))+1;
           % find index of each vertex in the faces it belongs (to
           % select the write quadrature points)
           vtx_idx = mod(find((F0(face_idx,:))'==i)-1,3)+1;
-          
+
           for m=1:size(vtx_idx,1)
-              
+
               if (vtx_idx(m)==1)
 %                   A(i,face_idx(m)) = A(i,face_idx(m)) + (-3/16)*area_initial(face_idx(m));
 %                   A(i,size(F0,1)+face_idx(m)) = A(i,size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
 %                   A(i,2*size(F0,1)+face_idx(m)) = A(i,2*size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
 %                   A(i,3*size(F0,1)+face_idx(m)) = A(i,3*size(F0,1)+face_idx(m)) + (55/144)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = face_idx(m);
                   Val(nnz_cur+1) = (-3/16)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+3) = i;
                   J(nnz_cur+3) = 2*size(F0,1)+face_idx(m);
                   Val(nnz_cur+3) = (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+4) = i;
                   J(nnz_cur+4) = 3*size(F0,1)+face_idx(m);
                   Val(nnz_cur+4) = (55/144)*area_initial(face_idx(m));
-                  
+
               elseif (vtx_idx(m)==2)
 %                   A(i,face_idx(m)) = A(i,face_idx(m)) + (-3/16)*area_initial(face_idx(m));
 %                   A(i,size(F0,1)+face_idx(m)) = A(i,size(F0,1)+face_idx(m)) + (55/144)*area_initial(face_idx(m));
 %                   A(i,2*size(F0,1)+face_idx(m)) = A(i,2*size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
 %                   A(i,3*size(F0,1)+face_idx(m)) = A(i,3*size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = face_idx(m);
                   Val(nnz_cur+1) = (-3/16)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = (55/144)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+3) = i;
                   J(nnz_cur+3) = 2*size(F0,1)+face_idx(m);
                   Val(nnz_cur+3) = (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+4) = i;
                   J(nnz_cur+4) = 3*size(F0,1)+face_idx(m);
                   Val(nnz_cur+4) = (5/72)*area_initial(face_idx(m));
-                  
+
               elseif (vtx_idx(m)==3)
 %                   A(i,face_idx(m)) = A(i,face_idx(m)) + (-3/16)*area_initial(face_idx(m));
 %                   A(i,size(F0,1)+face_idx(m)) = A(i,size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
 %                   A(i,2*size(F0,1)+face_idx(m)) = A(i,2*size(F0,1)+face_idx(m)) + (55/144)*area_initial(face_idx(m));
 %                   A(i,3*size(F0,1)+face_idx(m)) = A(i,3*size(F0,1)+face_idx(m)) + (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+1) = i;
                   J(nnz_cur+1) = face_idx(m);
                   Val(nnz_cur+1) = (-3/16)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+2) = i;
                   J(nnz_cur+2) = size(F0,1)+face_idx(m);
                   Val(nnz_cur+2) = (5/72)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+3) = i;
                   J(nnz_cur+3) = 2*size(F0,1)+face_idx(m);
                   Val(nnz_cur+3) = (55/144)*area_initial(face_idx(m));
-                  
+
                   I(nnz_cur+4) = i;
                   J(nnz_cur+4) = 3*size(F0,1)+face_idx(m);
                   Val(nnz_cur+4) = (5/72)*area_initial(face_idx(m));
-                  
+
               else
-                  
+
                   error('vertex index is not 1 nor 2 nor 3 nor 4')
-                  
+
               end
-              
+
               nnz_cur = nnz_cur+4;
-              
+
           end
-          
+
       end
-      
+
       I = nonzeros(I);
       J = nonzeros(J);
       Val = nonzeros(Val);
       A = sparse(I,J,Val);
-            
+
   else
-      
+
       error('specify quadrature order 1, 2 or 3 ')
-      
+
   end
-  
-  create_cache(cache_name);
-  
+
+  %create_cache(cache_name);
+
 end
-  
+
