edef510b8389a4021764f922e6552e5f7f0f7b0b
leokollersacht <leokollersacht@gmail.com>
2015-12-18 17:51:24 -0200

    fixed a bug on reinflation
    Former-commit-id: 9bd4a27e64c07ef757aefe910c3c21c243da32ac
    Former-commit-id: 08a14ac61517eb688be7143911953cccc9fb9cfa

--- Code/C/reinflate.cpp
+++ Code/C/reinflate.cpp
@@ -26,6 +26,8 @@ void reinflate(
   C = C_hat;
   MatrixXd C_prev = C; // needed for energies that depend on the previous step (e.g. DispStep)
   int step = 1;
+
+  // reinflation
   while (!H.empty()){
 
     // Intialize energy as infinity
@@ -42,6 +44,7 @@ void reinflate(
     	// First, find a feasible state (before stepping and projecting)
     	MatrixXd Uc = MatrixXd::Zero(C_hat.rows(), 3);
       filter(F,T,Uf,C,F_hat,Uc);
+      F = F+Uf;
       C = C+Uc;
       cout << "Reinflation step " << step << ": Energy = None (fesible state only)" << endl;
     }
@@ -69,6 +72,14 @@ void reinflate(
         gradient(C,C_hat,C_prev,F_hat,data,EnergyInflation,grad);
         MatrixXd Uc = -beta*grad;
         filter(F,T,Uf,C,F_hat,Uc);
+
+        // update fine mesh (it has to be bone here, beacuse
+        // otherwise the collision solver wilkl check intersections 
+        // between new coarse mesh and old fine mesh - they rarely intersect,
+        // but can be a problem)
+        F = F+Uf;
+        Uf = MatrixXd::Zero(F.rows(), 3);
+
         double new_energy = energy(C+Uc,C_hat,C_prev,F_hat,data,EnergyInflation);
         if (new_energy>current_energy)
         {
@@ -91,11 +102,64 @@ void reinflate(
       }
     }
 
-
-    // update
-    F = F+Uf;
     step = step+1;
     C_prev = C;
+
+  }
+
+  // final (optional) minimization
+  if (strcmp(EnergyFinal,"None")!=0)
+  {
+
+    ARAPData data; // precompute ARAP data for ARAP energies
+    if (strcmp(EnergyFinal,"SurfARAP")==0)
+    {
+      VectorXi b;
+      b.resize(0);
+      if (arap_precomputation(C_hat,F_hat,3,b,data))
+      {
+        cout << "ARAP precomputation successfully done " << endl;
+      }
+      else
+      {
+        cout << "ARAP precomputation failed, returning... " << endl;
+        return;
+      }
+    }
+
+    // Intialize energy as infinity
+    double current_energy = std::numeric_limits<double>::infinity();
+    double beta = 1e-2;
+
+    MatrixXd Uf = MatrixXd::Zero(F.rows(), 3);
+
+    while (true)
+      {
+        cout << "Final optimization. Energy = " << EnergyFinal  << endl;
+        MatrixXd grad;
+        gradient(C,C_hat,C_prev,F_hat,data,EnergyFinal,grad);
+        MatrixXd Uc = -beta*grad;
+        filter(F,T,Uf,C,F_hat,Uc);
+        double new_energy = energy(C+Uc,C_hat,C_prev,F_hat,data,EnergyFinal);
+        if (new_energy>current_energy)
+        {
+          beta = 0.5*beta;
+          cout << "energy increased, descreasing beta to " << beta << endl;
+          if (beta<1e-3)
+          {
+            cout << "beta too small. Quitting line search loop " << endl;
+            break;
+          }
+        }
+        else{
+          C = C+Uc;
+          current_energy = new_energy;
+          beta = 1.1*beta;
+          cout << "energy decreased to " << current_energy << ", increasing beta to " << beta << endl;
+        }
+        // If tiny step, then brak
+        if (((Uc).rowwise().norm()).norm()<1e-5) break;
+      }
 
   }
 
