5112e93274d94ab893d64c0be0e8be7e3e267388
leokollersacht <leokollersacht@gmail.com>
2015-12-22 19:25:14 -0200

    Added readme and more help on main function
    Former-commit-id: 20dd08eaa5155887c0d8390ba6e59abb47ea57b6
    Former-commit-id: 7d5be984c8e4beabbdc8da7e5278f1a63b6744f8

---
+++ Code/C/README.txt
@@ -0,0 +1,63 @@
+# Nested Cages 
+
+This code implements the following paper:
+Leonardo Sacht and Etienne Vouga and Alec Jacobson: [Nested Cages] (http://www.cs.columbia.edu/cg/nested-cages/) ACM Transactions on Graphics, vol. 34, no. 6 (SIGGRAPH Asia 2015). 
+
+It has been tested for Linux and Mac.
+
+If you have any comments or questions, please contact
+Leonardo Sacht by e-mail: leo@mtm.ufsc.br
+
+------------- Getting Nested Cages -------------
+git clone ??
+
+------------- Dependencies: -------------
+
+1) libigl
+git clone https://github.com/libigl/libigl.git
+Note: This is a header library, it doesn't need to be compiled (only included).
+
+2) CGAL
+Please install following the instructions at http://www.cgal.org/
+Note: We are using CGAL 4.7 in this project, not guaranteed to be compatible with other versions.
+
+3) Eigen
+Download from http://eigen.tuxfamily.org/index.php?title=Main_Page
+Note: this is a header-only library.
+
+4) Eltopo
+git clone https://github.com/leokollersacht/eltopo.git
+To compile, issue: 
+cd eltopo3d/
+make depend release
+Note: This is our fork of Eltopo, contains small changes specific to our project
+
+5) Tetgen
+Please install following the instructions at http://wias-berlin.de/software/tetgen/
+
+6) Meshfix
+Please download at http://sourceforge.net/projects/meshfix/
+
+------------- Compiling Nested Cages: -------------
+
+Once the above libraries are set (including changing their respecive cmake/findLIB file)
+our program is compiled using the following commands:
+
+cd nested_cages/
+cmake .
+make
+
+------------- Example usages -------------
+
+./nested_cages_demo
+Help information
+
+./nested_cages_demo fertility.off 2 8000r 1000r None Volume test
+Obtains 2 volume minimizing nested cages for fertility.off: one regular with 8000 faces and the other regular with 1000 faces. Output resulting cages to test_1.off and test_2.off
+
+./nested_cages_demo fertility.off 2 8000 1000 None Volume test
+The same as above, but outputs adaptive deimations (instead of regular)
+
+./nested_cages_demo fertility.off 2 fertilitiy_8000.off fertilitiy_1000.off SurfARAP None test
+Obtains 2 nested cages for fertility.off that minimize surface ARAP energy, using as input
+decimations fertilitiy_8000.off and fertilitiy_1000.off
--- Code/C/nested_cages.cpp
+++ Code/C/nested_cages.cpp
@@ -31,9 +31,19 @@ int main(int argc, char * argv[])
   using namespace igl::copyleft::cgal;
 
   if (argc==1){
-    cout<<"Usage: ./nested_cages [filename.(off)] q L(1) L(2) ... L(k)"<<endl;
-    cout<<"  q is the quadrature order for the shrinking flow"<<endl;
-    cout<<"  L(1) L(2) ... L(k) is the number of faces for each cage"<<endl;
+    cout << "Usage: ./nested_cages input.off q L(1) L(2) ... L(k) EnergyExpansion EnergyFinal output.off" << endl;
+    cout << "" << endl;
+    cout << "  q is the quadrature order for the shrinking flow" << endl;
+    cout << "" << endl;
+    cout << "  L(1) > L(2) > ... > L(k) is the number of faces for each cage" << endl;
+    cout << "  If L(k) is followed by 'r' the initial decimation for this cage will be regular (adaptive if no 'r')" << endl;
+    cout << "  Each L(k) can be replace by a filed with an input decimation " << endl;
+    cout << "" << endl;
+    cout << "  EnergyExpansion is the energy to be minimized for the re-inflation" << endl;
+    cout << "  Energies implemented: None, DispStep, DispInitial, Volume, SurfARAP, VolARAP " << endl;
+        cout << "" << endl;
+    cout << "  EnergyFinal is the energy to be minimized after the re-inflation (additional processing)" << endl;
+    cout << "  Energies implemented: None, DispStep, DispInitial, Volume, SurfARAP, VolARAP " << endl;
     return 0;
   }
   
@@ -44,10 +54,11 @@ int main(int argc, char * argv[])
   // quadrature order
   int quad_order = atoi(argv[2]);
 
+  // read input mesh
   Surface_mesh M; 
   std::ifstream is(argv[1]) ; is >> M ;
 
-  // // output input mesh as level 0
+  // output input mesh as level output_0.off
   char* filename; 
   char *suffix;
   if (asprintf(&filename, "%s%s", argv[argc-1], "_0.off")!=-1){
@@ -57,35 +68,49 @@ int main(int argc, char * argv[])
     return 0;
   }
 
-  // Converto CGAL's M to (V,F) format
+  // Converto CGAL's M to LibiIGL/Eigen (V,F) format
   MatrixXd V0;
   MatrixXi F0;
   polyhedron_to_mesh(M,V0,F0); 
 
-  // Fine mesh starts as the input one
+  // First fine mesh is the input mesh
   MatrixXd V = V0;
   MatrixXi F = F0;
 
+  // vector where each entry is the number of faces for eachj level
   int L[k];
+  // standard CGAL decimation is adaptive
   bool adaptive = true;
+  // declare input decimation
   Surface_mesh M_hat;
+
+  // Loop over levels
   for(int i = 0;i<k;i++){
+    // check if argv[i+3] is a valid file. If so, it will be used as input decimation 
     std::ifstream is_file(argv[i+3]);
-    // check if argv is a valid file
-    if (is_file){
+    if (is_file)
+    {
       is_file >> M_hat;
-    } else{
+    } 
+    // otherwise the input decimation is computed by decimating the previous layer
+    // with CGAL decimation (regular or adaptive)
+    else
+    {
       // first check if last charcater of argv[i+2] is r. If it is, drop the 'r' and adaptive = false
       adaptive = remove_all_chars_and_count(argv[i+3], 'r')==0;
-      // else check if argv[i+2] is a valid integer (throw an error if it is not)
+      // check if argv[i+2] is a valid integer (throw an error if it is not)
       if (!legal_int(argv[i+2])){
         cout << "you have to pass integer values or valid input deimatations"  << endl;
         cout << "the invalid argument you have passed is " << argv[i+3] << endl;
         return 0;
       }
+      // specified number of faces for this cage
       L[i] = atoi(argv[i+3]);
+      // value to pass to the decimator
       float ratio = (1.*L[i])/(1.*M.size_of_facets()); 
+      // the previously computed will be decimated
       M_hat = M;
+      // decimate
       decimate_CGAL(&M_hat,ratio,adaptive);
     }
 
@@ -93,7 +118,9 @@ int main(int argc, char * argv[])
     // (replace by Meshfix in the future)
 	  MatrixXd V_coarse;
     MatrixXi F_coarse;
+    // Cnvert decimation to LibIGL/Eigen format
     polyhedron_to_mesh(M_hat,V_coarse,F_coarse); 
+    // Parameters to call function to check for decimation's self-intersections
     RemeshSelfIntersectionsParam params;
     params.detect_only = true;
     params.first_only = true;
@@ -103,11 +130,11 @@ int main(int argc, char * argv[])
     VectorXi J;
     VectorXi IM;
     remesh_self_intersections(V_coarse,F_coarse,params,tempV,tempF,IF,J,IM);
+    // If input coarse mesh self-intersect, remove self-intersections with Meshfix (to-do)
     if (IF.rows()>0){
     	cout << i+1 << "-th input decimation self-intersects. Quitting...  " << endl;  
     	return 0;
     }
-
 
 	  // calculate triangle areas for initial mesh (will be used
 	  // to define the integral at _every_ step - the metric is fixed)
@@ -117,20 +144,22 @@ int main(int argc, char * argv[])
     // Precompute matrix that convert gradients at quadrature points to gradients at mesh vertices
   	SparseMatrix<double> A_qv;
     gradQ_to_gradV(V, F, area_0, quad_order, A_qv);
-  	// Flow M inside M_hat
+  	// Flow M inside M_hat and save the result to a stack M of flow meshes
     stack<MatrixXd> H;
     flow_fine_inside_coarse(V,F,V_coarse,F_coarse,A_qv,H);
 
-    // Reinflate
+    // Reinflate and output to cage to C
     MatrixXd C;
     reinflate(H,F,V_coarse,F_coarse,argv[argc-3],argv[argc-2],C);
 
+    // sanity check: cage should never self-intersect at this stage
     remesh_self_intersections(C,F_coarse,params,tempV,tempF,IF,J,IM);
     if (IF.rows()>0){
       cout << i+1 << "-th output cage self-intersects. ERROR! Quitting...  " << endl;  
       return 0;
     }
 
+    // sanity check: cage should never intersect input decimation
     intersect_other(C,F_coarse,V,F,true,IF);
     if (IF.rows()>0){
       cout << i+1 << "-th output cage intersect previous cage. ERROR! Quitting...  " << endl;  
@@ -143,7 +172,7 @@ int main(int argc, char * argv[])
     V = C;
     F = F_coarse;
 
-    // Ouput cage
+    // Output cage to file output_i.off
     if ((asprintf(&suffix,"_%d.off",i+1)!=-1) && (asprintf(&filename, "%s%s", argv[argc-1], suffix)!=-1)){
       writeOFF(filename,C,F_coarse);
     } else {
